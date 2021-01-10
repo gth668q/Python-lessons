c55a68ce72719b82a8d41a919e2b93872aeec379
Moazzam Khan <khanmo@us.ibm.com>
2021-01-10 18:06:06 -0500

    adding more methods

--- Lession1-Strings.py
+++ Lession1-Strings.py
@@ -1,6 +1,8 @@
 
 example_string  = "HELLO WORLD HELLO HELLO"
 
-count_string = example_string.find('HELLO',  len(example_string)-5)
+count_string = example_string.count('HELLO',  len(example_string)-5)
+
+find_string = example_string.find('HELLO',  len(example_string)-5)
 
 print(count_string)
