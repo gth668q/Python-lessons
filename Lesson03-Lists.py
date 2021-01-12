85c5261998b70a6de73a56138368653574d3b039
Moazzam Khan <khanmo@us.ibm.com>
2021-01-12 13:00:16 -0500

    Code for list, tuple, set

---
+++ Lesson03-Lists.py
@@ -0,0 +1,65 @@
+
+
+my_shopping_list = ["Milk", "Bread", "Eggs", "Milk", "Bread"]
+
+my_game_score = [ 20, 30, 10]
+
+
+#my_shopping_list.append("Soda")
+#print(my_shopping_list)
+
+#my_shopping_list.insert(1, "Cheese")
+#print(my_shopping_list)
+
+#print(len(my_shopping_list))
+#print(my_shopping_list[-1])
+
+#my_shopping_list.reverse()
+#print(my_shopping_list)
+
+
+'''for item in my_shopping_list:
+    print("The next element in the list is:")
+    print(item)
+    print(" Just another print")
+
+print("Last print")'''
+
+'''for item in my_game_score:
+    item = item + 10
+    print(item)'''
+
+
+'''for index, item in enumerate(my_shopping_list, start=1):
+    print(index, item)'''
+
+
+
+# Tuple Example
+
+#my_shopping_tuple = ("Milk", "Bread", "Eggs")
+
+
+
+#my_shopping_list[0] = "Butter"
+
+#print(my_shopping_list)
+
+#my_shopping_tuple[0] = "Butter"
+
+
+# Set Example
+
+my_shopping_set = {"Milk", "Bread", "Eggs", "Milk"}
+
+my_shopping_set2 = { "Mangoes", "Apples", "Milk"}
+
+my_new_set.difference( my_shopping_set2)
+
+print(unique_element)
+
+
+my_new_set = set(my_shopping_list) # type casting
+
+print(my_new_set)
+
