0fac1c9f73274b73a99d2a6613bdc443435e249e
Moazzam Khan <khanmo@us.ibm.com>
2021-01-13 13:04:51 -0500

    Code for dictionary

---
+++ Lesson04-Dictionary.py
@@ -0,0 +1,45 @@
+
+
+student = {"name":"John", "age":22, "courses":["python", "c++","os"] }
+
+
+#print(student["courses"])
+
+#print(student.keys())
+
+#print(student.values())
+
+#print(student.items())
+
+#print(student.get("Grade", "Not Found"))
+
+#age_variable = student.pop("age")
+
+#print(student.keys())
+
+#student.update({"age":30})
+
+#print(student.values())
+
+
+#for value in student.values():
+#    print(value)
+
+#name1 = "Edward"
+#name2 = "Edward"
+
+age = 1
+
+
+
+if age >10:
+
+    print(" Age is greater than 10")
+
+elif age < 10 and age > 5:
+
+    print(" Age is between 5 and 10")
+
+else:
+
+    print("Age is either 5 or less than 5")
