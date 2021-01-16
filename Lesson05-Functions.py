98c6879893ed62f1ef46d18ec589f20e97e1710a
Moazzam Khan <khanmo@us.ibm.com>
2021-01-16 13:09:44 -0500

    Code for Functions

---
+++ Lesson05-Functions.py
@@ -0,0 +1,75 @@
+
+groceries = [ "Apples", "oranges", "Bananas"]
+
+
+# for item in groceries:
+#     print(item)
+
+#
+# age = 5
+# number = 0
+#
+# while number <= age:
+#
+#     number += 1
+#     if number == 3:
+#         continue
+#     print("In While loop")
+
+#
+# def printString():          #Function Definition
+#     print("Hello World!")
+
+# number =1
+#
+# while number < 5:
+#     printString()
+#     number += 1
+
+#
+# def functionWithParameter(name):
+#     print("Hello"+' ' +name)
+#
+# functionWithParameter("Mo")
+
+
+# def addNumbers(numberone, numbertwo):
+# #
+# #     sum = numberone + numbertwo
+# #
+# #     return sum
+# #
+# #
+# #
+# # print(addNumbers(55, 100))
+
+#
+# def unknownParameter(*person):
+#
+#     print(person[0:3])
+#
+# unknownParameter("Edward" , 30, "Atlanta")
+
+# def unorderedFunction( city, age, name="John Doe"):
+#
+#     resulting_string = 'The name of the person is {}, his age is {} and he lives in {}'.format(name, age, city)
+#
+#     print(resulting_string)
+#
+#
+# unorderedFunction(age=15, city = "Chicago", name="Max")
+#
+# unorderedFunction(age=15, city = "Chicago")
+
+# def anotherFunction(**person):
+#
+#     # print(person['name'])
+#     # print(person['age'])
+#     # print(person['city'])
+#
+#     for i in person.keys():
+#         print(person[i])
+#
+#
+#
+# anotherFunction(age=15, city = "Chicago", name="Max")
